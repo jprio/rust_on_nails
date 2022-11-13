530b5066a699e3aa771d9c2c73aa72132a6a0c7a
Jean-Pierre RIO <jeanpierre.rio@gmail.com>
2022-11-13 08:29:16 +0000

    axum

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,11 @@
 mod config;
+mod errors;
+
+use crate::errors::CustomError;
+use axum::{extract::Extension, response::Json, routing::get, Router};
+use deadpool_postgres::Pool;
+use queries::fortunes::Fortunes;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
@@ -6,15 +13,27 @@ async fn main() {
 
     let pool = config.create_pool();
 
-    let client = pool.get().await.unwrap();
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(fortunes))
+        .layer(Extension(config))
+        .layer(Extension(pool.clone()));
 
-    let fortunes = queries::fortunes::fortunes()
-        .bind(&client)
-        .all()
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
         .await
         .unwrap();
+}
+#[axum_macros::debug_handler]
+async fn fortunes(Extension(pool): Extension<Pool>) -> Result<Json<Vec<Fortunes>>, CustomError> {
+    let client = pool.get().await?;
 
-    dbg!(fortunes);
+    let fortunes = queries::fortunes::fortunes().bind(&client).all().await?;
+
+    Ok(Json(fortunes))
 }
 
 // Include the generated source code
